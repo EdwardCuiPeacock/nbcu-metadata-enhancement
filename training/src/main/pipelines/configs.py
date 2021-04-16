4b4bcd87c987cd72c74e86c597f246fb0eb545f7
EdwardCuiPeacock <Edward.Cui@nbcuni.com>
2021-04-16 17:20:08 -0400

    use old docker file

--- training/src/main/pipelines/configs.py
+++ training/src/main/pipelines/configs.py
@@ -41,7 +41,7 @@ IMAGE = 'gcr.io/' + GOOGLE_CLOUD_PROJECT + '/metadata-dev-pipeline-base'
 #       eventually we'll want to do this at RUN time, since this 
 #       will allow us to template out any temporal aspects of the query
 
-file_loader = FileSystemLoader('src/main/queries')
+file_loader = FileSystemLoader('main/queries')
 env = Environment(loader=file_loader)
 template = env.get_template('ingest_query.sql')
 
@@ -69,7 +69,7 @@ def get_domain_size(schema_path, feature):
 
     return len(domain.value)
 
-num_labels = get_domain_size('src/schema/schema.pbtxt', 'tags')
+num_labels = get_domain_size('schema/schema.pbtxt', 'tags')
 
 
 ## TRAINING ARGS

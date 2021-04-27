ba7c6d18c27832f4a168352106bbfddde4a7a706
EdwardCuiPeacock <Edward.Cui@nbcuni.com>
2021-04-27 12:25:38 -0400

    m

--- training/src/model_run.sh
+++ training/src/model_run.sh
@@ -3,6 +3,11 @@ ENDPOINT=df6bc4688870067-dot-us-east1.pipelines.googleusercontent.com
 CUSTOM_TFX_IMAGE=gcr.io/${GOOGLE_CLOUD_PROJECT}/tfx-pipeline
 PIPELINE_NAME=metadata_dev_edc_base_0_0_4
 
+# For local machine
+# gcloud init # initialize and login if not already
+# gcloud auth login # logging from webpage
+# gcloud auth configure-docker # add credential to docker
+
 alias build_pipeline_local="tfx pipeline create --pipeline-path=./local_runner.py"
 alias update_pipeline_local="tfx pipeline update --pipeline-path=./local_runner.py"
 alias run_pipeline_local="tfx run create --pipeline_name$PIPELINE_NAME"

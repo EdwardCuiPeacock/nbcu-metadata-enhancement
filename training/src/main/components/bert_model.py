f61b93c0d25cfa397e2188851b485bd6dd1af5ae
EdwardCuiPeacock <Edward.Cui@nbcuni.com>
2021-04-26 22:10:30 -0400

    m

--- training/src/main/components/bert_model.py
+++ training/src/main/components/bert_model.py
@@ -95,7 +95,7 @@ def get_compiled_model(num_labels, seq_length):
         # clipnorm only seems to work in TF 2.4 with distribution strategy
         model.compile(
             optimizer="adam",
-            loss="cosine_similarity",
+            loss="binary_crossentropy",
             #metrics=metrics,
         )
     return model

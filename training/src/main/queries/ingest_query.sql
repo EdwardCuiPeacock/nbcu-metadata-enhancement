44a8f09fefbff6c42f9d2365996c1c6554c5fa79
EdwardCuiPeacock <Edward.Cui@nbcuni.com>
2021-05-03 15:07:01 -0400

    fix query

--- training/src/main/queries/ingest_query.sql
+++ training/src/main/queries/ingest_query.sql
@@ -5,12 +5,12 @@ SELECT
   keywords AS tokens, tags
 FROM `{{ DATA_SOURCE_TABLE }}`
 
-UNION
+UNION ALL
 
 SELECT
   ARRAY_TO_STRING(ARRAY(
     SELECT * 
-        FROM UNNEST(SPLIT(program_longsynopsis, " ")) LIMIT {{ TOKEN_LIMIT }}), " ") as synopsis,
+        FROM UNNEST(SPLIT(TitleDetails_longsynopsis, " ")) LIMIT {{ TOKEN_LIMIT }}), " ") as synopsis,
   tokens, labels AS tags
 FROM `res-nbcupea-dev-ds-sandbox-001.metadata_enhancement.test_titles_synopsis_keywords_all_labels`
 

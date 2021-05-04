9820003f2064420bbfa8e6d6939a7290bcfc6a73
EdwardCuiPeacock <Edward.Cui@nbcuni.com>
2021-05-04 10:53:50 -0400

    Try run full

--- training/src/main/components/transform.py
+++ training/src/main/components/transform.py
@@ -76,7 +76,7 @@ def preprocessing_fn(inputs, custom_config):
     #     deferred_vocab_filename_tensor=tf.constant("gs://metadata-bucket-base/tfx-metadata-dev-pipeline-output/metadata_dev_edc_base_0_0_3/Transform/transform_graph/20890/transform_fn/assets/tags"),
     #     num_oov_buckets=1,
     #     )
-    vocab_file = tf.constant(custom_config["token_vocab_list"])
+    # vocab_file = tf.constant(custom_config["token_vocab_list"])
     # tokens = tft.apply_vocabulary(tokens, 
     #    deferred_vocab_filename_tensor=vocab_file,
     #    num_oov_buckets=0)

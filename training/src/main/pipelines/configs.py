177234a5041687ee89403388a10b458d93bcca0a
EdwardCuiPeacock <Edward.Cui@nbcuni.com>
2021-05-04 13:48:18 -0400

    try synopsis tokens

--- training/src/main/pipelines/configs.py
+++ training/src/main/pipelines/configs.py
@@ -34,7 +34,7 @@ except ImportError:
 OUTPUT_TABLE = "res-nbcupea-dev-ds-sandbox-001.metadata_enhancement.model_results"
 GOOGLE_CLOUD_PROJECT = "res-nbcupea-dev-ds-sandbox-001"
 BQ_DATASET = 'metadata_enhancement'
-BQ_TABLE = 'test_titles_synopsis_keywords_all_labels'#merlin_data_with_lang_type_keywords, 'synopsis_dylan_150tag_with_tokens_and_keywords'
+BQ_TABLE = 'synopsis_dylan_150tag_with_tokens_and_keywords'#merlin_data_with_lang_type_keywords, 'synopsis_dylan_150tag_with_tokens_and_keywords'
 DATA_SOURCE_TABLE = f"{GOOGLE_CLOUD_PROJECT}.{BQ_DATASET}.{BQ_TABLE}"
 
 TOKEN_LIMIT = 256
@@ -84,14 +84,14 @@ num_labels = int(client.query(query_labels).to_dataframe()["labels_count"].value
 client = bigquery.Client()
 query_tokens = f"""
     SELECT 
-        --MAX(tokens_length) AS tokens_length,
-        MAX(keyword_length) AS keywords_length
+        MAX(tokens_length) AS tokens_length,
+        -- MAX(keyword_length) AS keywords_length
     FROM `{DATA_SOURCE_TABLE}`
 """
 token_counter = client.query(query_tokens).to_dataframe()
 # buffer space for raggedtensor to dense tensor
-# N2V_TOKEN_LENGTH = int(token_counter["tokens_length"].values)
-N2V_KEYWORD_LENGTH = int(token_counter["keywords_length"].values)
+N2V_TOKEN_LENGTH = int(token_counter["tokens_length"].values)
+# N2V_KEYWORD_LENGTH = int(token_counter["keywords_length"].values)
 ## TRAINING ARGS
 USE_STEPS = False
 TRAIN_NUM_STEPS = 10000
@@ -110,7 +110,7 @@ custom_config = {
     'use_steps': USE_STEPS,
     'seq_length': TOKEN_LIMIT,
     'token_vocab_list': "gs://edc-dev/kubeflowpipelines-default/tfx_pipeline_output/node2vec_sports_syn_0_1_0/Transform/transform_graph/18561/transform_fn/assets/node_vocab_txt",
-    'max_token_length': N2V_KEYWORD_LENGTH,#N2V_TOKEN_LENGTH,
+    'max_token_length': N2V_TOKEN_LENGTH,
     #'max_keyword_length': N2V_KEYWORD_LENGTH,
 }
 

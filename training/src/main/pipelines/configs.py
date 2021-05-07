76164f7a98a4ccd8c018a77748a5777cc0f16dd8
EdwardCuiPeacock <Edward.Cui@nbcuni.com>
2021-05-07 18:11:13 -0400

    disable cache

--- training/src/main/pipelines/configs.py
+++ training/src/main/pipelines/configs.py
@@ -40,7 +40,7 @@ DATA_SOURCE_TABLE = f"{GOOGLE_CLOUD_PROJECT}.{BQ_DATASET}.{BQ_TABLE}"
 TOKEN_LIMIT = 256
 TEST_LIMIT = None
 
-enable_cache = True
+enable_cache = False
 USE_AI_PLATFORM = True
 
 IMAGE = 'gcr.io/' + GOOGLE_CLOUD_PROJECT + '/edc-dev-pipeline'

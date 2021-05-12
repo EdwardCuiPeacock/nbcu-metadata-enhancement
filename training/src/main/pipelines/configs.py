9dcccd794f84162f5b6fd10bd4a510e82f66f48d
EdwardCuiPeacock <Edward.Cui@nbcuni.com>
2021-05-12 18:10:23 -0400

    m

--- training/src/main/pipelines/configs.py
+++ training/src/main/pipelines/configs.py
@@ -38,10 +38,10 @@ BQ_TABLE = 'synopsis_genres_167_clustered_tags' #'synopsis_dylan_150tag_with_tok
 DATA_SOURCE_TABLE = f"{GOOGLE_CLOUD_PROJECT}.{BQ_DATASET}.{BQ_TABLE}"
 
 TOKEN_LIMIT = 256
-TEST_LIMIT = 20
+TEST_LIMIT = None
 
 enable_cache = False
-USE_AI_PLATFORM = False
+USE_AI_PLATFORM = True
 
 IMAGE = 'gcr.io/' + GOOGLE_CLOUD_PROJECT + '/edc-dev-pipeline'
 
